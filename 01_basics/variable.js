5a0a25ca0b40d43d182ebfa4301db35d3383930c
bishtn864-ux <bishtn864@gmail.com>
2026-06-03 21:49:31 +0530

    variables

---
+++ 01_basics/variable.js
@@ -0,0 +1,17 @@
+const accountId=12234;
+let accountEmail="example.gmail.com";
+var accountPassword="12345";
+accountCity="jaipur";
+let accountState;
+
+//accountId=2; // not aalowed
+accountEmail="nbisht9090.gmail.com";
+accountPassword="4567"
+accountCity="mumbai"
+
+/*
+prefer not to use var because of issue in block scope and function scope
+*/
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
