c1061d045acefa338aabad248830aaf4364ad688
bishtn864-ux <bishtn864@gmail.com>
2026-06-03 22:11:48 +0530

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; //treat all js code as newer version 
+
+//alert("hello");  we are using nodejs,not browser
+
+//undefiend is declaration but giving the value but null is a standalone value it is a datatype
+
+console.log(typeof null);
+
