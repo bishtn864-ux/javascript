7005375fedf2352e6f790b06819941cb29c15d07
bishtn864-ux <bishtn864@gmail.com>
2026-06-04 20:38:25 +0530

    converstion

---
+++ 01_basics/converstionoperation.js
@@ -0,0 +1,16 @@
+let score="33";
+
+console.log(typeof score );
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber );
+
+
+//"33"=>33
+//"33abc"=>NaN
+//"null"=>0
+
+let isLockedIn=1;
+let boolenIsLoggedIn=Boolean(isLockedIn);
+
+console.log(boolenIsLoggedIn);
