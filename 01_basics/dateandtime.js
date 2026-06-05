afabf6a344eec5788eb27735ac3fe6d7c3765ba0
bishtn864-ux <bishtn864@gmail.com>
2026-06-05 23:48:04 +0530

    day3

---
+++ 01_basics/dateandtime.js
@@ -0,0 +1,28 @@
+//dates
+
+let mydate=new Date();
+/*console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+console.log(typeof mydate);
+
+let mycreatedate=new Date(2023,0,23,5,5);
+console.log(mycreatedate.toString());
+
+let mycreatedateone=new Date("2023-01-14");
+console.log(mycreatedateone.toLocaleDateString());
+
+let mytimestamp=Date.now();
+console.log(mytimestamp);
+console.log(mycreatedate.getTime());
+
+const nikhil=new Date();
+
+console.log(Math.floor(Date.now()/1000));
+
+*/
+
+console.log(mydate);
+console.log(mydate.getMonth());
+console.log(mydate.getDay());
+console.log(mydate.getFullYear());
---
+++ 01_basics/nummath.js
@@ -0,0 +1,36 @@
+/*const score=400;
+
+const balance=new Number(100);
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+
+const othherNumber=23.3589;
+
+console.log(othherNumber.toPrecision(3));
+
+const hunderds=1000000;
+console.log(hunderds.toLocaleString('en-IN'));
+
+//******************maths 
+
+console.log(Math);
+
+console.log(Math.abs(-1));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.9));
+console.log(Math.floor(4.9));
+console.log(Math.sqrt(4));
+console.log(Math.min(5,7,9,9));
+console.log(Math.max(6,3,2));
+*/
+
+
+console.log(Math.random()); //used in dice game
+console.log(Math.floor((Math.random()*10 )+1));
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min +1)+min));
