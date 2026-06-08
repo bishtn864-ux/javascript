09451d465ddce09f6f2de13d2f58c8a95e2d6347
bishtn864-ux <bishtn864@gmail.com>
2026-06-08 22:26:58 +0530

    fucntion

---
+++ 03_basics/function.js
@@ -0,0 +1,19 @@
+function saymyname (){
+    console.log("h");
+    console.log("i");
+    console.log("t");
+}
+
+saymyname();
+
+function addTwonumber(number1,number2){
+    let result=number1+number2;
+}
+//console.log(result);
+
+function calculateCartprice(...num1){
+      return num1;
+}
+
+console.log(calculateCartprice(200,400,300))
+
